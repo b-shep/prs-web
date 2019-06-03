ceb82a51895aa82d2010ef6fcce3262a2ae5e6a2
b-shep <b.shep.git@gmail.com>
2019-06-03 09:51:37 -0400

    test updates

--- src/test/java/com/prs/business/PurchaseRequestTests.java
+++ src/test/java/com/prs/business/PurchaseRequestTests.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.prs.db.PurchaseRequestRepository;
 import com.prs.db.UserRepository;
+import com.prs.web.PurchaseRequestController;
 
 @Transactional
 @SpringBootTest
@@ -28,27 +29,37 @@ public class PurchaseRequestTests {
 	PurchaseRequestRepository prRepo;
 
 	@Test
-	public void testPurchaseRequestsGetAll() {
+	public void testPurchaseRequestGetAll() {
 		User u = userRepo.findById(4).get();
-		System.out.println(u);
 		PurchaseRequest pr = new PurchaseRequest(0, u, "testDescrip", "justificationtest", LocalDate.now() , "car", "NEW", 100, LocalDateTime.now(), "null");
 		prRepo.save(pr);
 		Iterable <PurchaseRequest> prs = prRepo.findAll();
 		assertNotNull(prs);
 	}
 	
+//	@Test
+//	public void testPurchaseRequestUpdate() {
+//		User u = userRepo.findById(4).get();
+//		PurchaseRequest pr = new PurchaseRequest(0, u, "testDescrip", "justificationtest", LocalDate.now() , "car", "NEW", 100, LocalDateTime.now(), "null");
+//		prRepo.save(pr);
+//		PurchaseRequestController prcon = new PurchaseRequestController();
+//		PurchaseRequest updatePr = new PurchaseRequest(0, u, "TESTupdate", "justificationtest", LocalDate.now() , "car", "NEW", 100, LocalDateTime.now(), "null");
+//		prcon.update(updatePr);
+//		Iterable<PurchaseRequest> prs = prRepo.findAll();
+//		System.out.println(prs);
+//		PurchaseRequest check = prRepo.findById(2).get();
+//		
+//		String description = check.getDescriptiton();
+//		assertEquals("TESTupdate", description);
+//	}
+	
 	@Before
 	public void testPurchaseRequestAddAndDelete() {
 		User u = userRepo.findById(4).get();
-		System.out.println(u);
 		PurchaseRequest pr = new PurchaseRequest(0, u, "testDescrip", "justificationtest", LocalDate.now() , "car", "NEW", 100, LocalDateTime.now(), "null");
 		assertNotNull(prRepo.save(pr));
 	}
 	
-	 
-//	User u = userRepo.findById(1).get();
-//	PurchaseRequest pr = new PurchaseRequest(0, u, "testDescrip", "justificationtest", LocalDate.now() , "car", "NEW", 100, LocalDateTime.now(), "null");
-//	assertNotNull(prRepo.save(pr));
 	
 
 }
